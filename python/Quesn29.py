d40ce300f9afce463836f509577bd6fbe15c85f3
harshavardhan784 <98533767+harshavardhan784@users.noreply.github.com>
2022-10-23 19:36:29 +0530

    Add files via upload

---
+++ python/Quesn29.py
@@ -0,0 +1,15 @@
+def rotate(ar, d, n):
+    if(d>n):
+        d=d%n
+    l=[]
+    k=ar.index(d)
+    l=ar[k+1:]+ar[0:k+1]
+    return l
+        
+n=int(input("Array Size:"))
+ar=[]
+print("Enter the elements")
+for i in range(n):
+    ar.append(int(input()))
+d=input("Enter d value:")
+print(rotate(ar,int(d),n))
