ee493bb66ac65b140ed1fb20e0bc0a639494af4d
harshavardhan784 <98533767+harshavardhan784@users.noreply.github.com>
2022-10-30 11:51:04 +0530

    Create Quesn9.py

---
+++ python/Quesn9.py
@@ -0,0 +1,5 @@
+for i in range(1000,3000):
+    if(i%2==0 and i%20<=9 and i%200 <=99 and i%2000<=999):
+        print(i,end="")
+    if(i<2888 and i%2==0 and i%20<=9 and i%200 <=99 and i%2000<=999):
+        print(",",end="")
